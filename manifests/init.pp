9796841de72e97fcf293bb2d6cc9097e18c8b22d
mh <mh@immerda.ch>
2008-09-13 10:11:47 +0000

    added gpg module

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,36 +1,18 @@
-# modules/skeleton/manifests/init.pp - manage skeleton stuff
+# modules/gpg/manifests/init.pp - manage skeleton stuff
 # Copyright (C) 2007 admin@immerda.ch
 # GPLv3
 
-# modules_dir { "skeleton": }
+# modules_dir { "gpg": }
 
-class skeleton {
+class gpg {
     case $operatingsystem {
-        gentoo: { include skeleton::gentoo }
-        default: { include skeleton::base }
+        gentoo: { include gpg::gentoo }
+        default: { include gpg::base }
     }
 }
 
-class skeleton::base {
-    package{'skeleton':
+class gpg::base {
+    package{'gnupg':
         ensure => installed,
     }
-
-    service{skeleton:
-        ensure => running,
-        enable => true,
-        #hasstatus => true, #fixme!
-        require => Package[skeleton],
-    }
-
-}
-
-class skeleton::gentoo inherits skeleton::base {
-    Package[skeleton]{
-        category => 'some-category',
-    }
-
-    #conf.d file if needed
-    # needs module gentoo
-    #gentoo::etcconfd { skeleton: require => "Package[skeleton]", notify => "Service[skeleton]"}
 }
