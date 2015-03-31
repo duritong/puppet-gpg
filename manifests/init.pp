54c0673c98f84b2b76f9943b1d47610e88992fa9
mh <mh@immerda.ch>
2015-03-31 21:42:39 +0200

    make it EL7 ready

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,15 +1,15 @@
 # modules/gpg/manifests/init.pp - manage gpg stuff
 # Copyright (C) 2007 admin@immerda.ch
 # GPLv3
-
 class gpg {
-    case $::operatingsystem {
-        centos: {
-            case $::lsbmajdistrelease {
-                6: { include gpg::centos6 }
-                default: { include gpg::base }
-            }
-        }
-        default: { include gpg::base }
+  case $::operatingsystem {
+    'CentOS': {
+      if versioncmp($::operatingsystemmajrelease,'5') > 0 {
+        include gpg::centos6
+      } else {
+        include gpg::base
+      }
     }
+    default: { include gpg::base }
+  }
 }
